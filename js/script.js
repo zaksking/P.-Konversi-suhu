df809b939cc98dad0a1a918db55c4ab049c70cfa
habibirakatauw <zakskinglop4@gmail.com>
2024-05-05 21:52:27 +0700

    main

--- js/script.js
+++ js/script.js
@@ -2,15 +2,18 @@ function ConvertToFahrenhait() {
     var celcius = parseFloat(document.getElementById('askk').value);
     var Fahrenheitanswer = (celcius *9/5) + 32;
     document.getElementById('answer').value = Fahrenheitanswer.toFixed(2);
+    document.getElementById('caranya').value = "(" + celcius + " x 9 / 5) + 32 = " + Fahrenheitanswer.toFixed(2)
 }
 
 function ConvertToCelcius() {
     var fahrenhait = parseFloat(document.getElementById('askk').value);
     var celciusanswer = (fahrenhait - 32) *5/9;
     document.getElementById('answer').value = celciusanswer.toFixed(2);
+    document.getElementById('caranya').value = "(" + fahrenhait + " - 32) x 5 / 9 = " + celciusanswer.toFixed(2)
 }
 
 function DsReset() {
     document.getElementById('askk').value = null;
     document.getElementById('answer').value = null;
+    document.getElementById('caranya').value = null;
 }
